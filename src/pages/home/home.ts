b09dae4529ee1de9a491cc807e46b0cbb195c7a9
Mario Serrano <maseflores79@gmail.com>
2018-10-06 09:45:25 -0700

    Cargando lista de animales

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ANIMALES} from '../../data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
 
+  constructor() {
+    /* Crea un clon de los datos en ANIMALES porque 
+    si lo asigno directamente, todo cambio en this.animales 
+    afectará a ANIMALES. */
+      this.animales = ANIMALES.slice(0);
   }
 
 }
